0bdce744af7779ac2e0917ca28faf01b379fd8bd
chungyoojin <134499664+chungyoojin@users.noreply.github.com>
2023-05-31 21:00:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,7 @@ if st.button('피드백 받기'):
     st.write(response)
     if label[0] == 1:
             if label[1] == 1:
-                            if label[2] == 1: 
-                            st.success('다항식의 나눗셈을 잘하는구나!', icon="✅")
+                            if label[2] == 1: st.success('다항식의 나눗셈을 잘하는구나!', icon="✅")
                         else : st.info('다항식의 나눗셈을 복습해볼까?', icon="ℹ️")
             st.success('거듭제곱의 곱셈을 잘하는구나!', icon="✅")
             else: st. info('거듭제곱의 곱셈을 복습해볼끼?', icon="ℹ️")
