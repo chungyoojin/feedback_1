7618088495900ff30bde06fba3363465bd418ccc
chungyoojin <134499664+chungyoojin@users.noreply.github.com>
2023-06-04 23:55:56 +0900

    Update 1.py

--- pages/1.py
+++ pages/1.py
@@ -3,11 +3,9 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.title("자동 채점 및 피드백 모델")
 st.write("**조원** : 김명식, 김재훈, 김지영, 신인섭, 윤예린, 정유진")
-st.header("설명추가")
-    
-st.title("문항 설계의 전반적인 원리")
-st.subheader("문항 설계 목표")
-st.write("1. 식의 계산 단원에서 학생들이 가지고 있는 인지 요소 분석")
+st.subheader("문항 설계의 전반적인 원리")
+st.write("**문항 설계 목표**")
+st.write("\n\n 1. 식의 계산 단원에서 학생들이 가지고 있는 인지 요소 분석")
 st.write("2. 식의 계산 단원에서 학생들이 가지고 있는 오개념 및 오류 분석")
 
 st.subheader("식의 계산 단원 내 지식맵 구성")
