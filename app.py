1fd6d389be3ae5e2a4635af6380385679a2ac4fb
chungyoojin <134499664+chungyoojin@users.noreply.github.com>
2023-06-04 23:34:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -85,3 +85,23 @@ if st.button('모범답안', key='button2_7_4'):
     st.image(image_path, caption='2-7모범답안')
 
     
+    
+    
+    
+    
+    
+    
+    
+st.title("✨문항 설계의 전반적인 원리")
+st.subtitle("문항 설계 목표")
+st.write("1. 식의 계산 단원에서 학생들이 가지고 있는 인지 요소 분석")
+st.write("2. 식의 계산 단원에서 학생들이 가지고 있는 오개념 및 오류 분석")
+
+st.subtitle("식의 계산 단원 내 지식맵 구성")
+image_path = "save/2-7 모범답안.png-.png"
+st.image(image_path, caption='2-7모범답안')
+
+
+
+st.write("여기 누군가 적어주세요ㅠㅠ"
+    
