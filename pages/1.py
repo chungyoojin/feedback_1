1be893581a26d775f8ec83b7f7c32873b248ea56
chungyoojin <134499664+chungyoojin@users.noreply.github.com>
2023-06-04 23:46:52 +0900

    Update 1.py

--- pages/1.py
+++ pages/1.py
@@ -1,4 +1,9 @@
+import streamlit as st
 
+st.set_page_config(layout="wide")
+st.title("자동 채점 및 피드백 모델")
+st.write("**조원** : 김명식, 김재훈, 김지영, 신인섭, 윤예린, 정유진")
+st.header("설명추가")
     
 st.title("문항 설계의 전반적인 원리")
 st.subtitle("문항 설계 목표")
